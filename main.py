9cde094e00fac7747dad86488271a8b10b0c3bc8
Mike Wang <mikewang1@icloud.com>
2024-04-19 14:09:52 +0200

    implemented reinforce

--- main.py
+++ main.py
@@ -6,36 +6,110 @@ from collections import namedtuple, deque
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.distributions.categorical import Categorical
+from torch.optim import Adam, Optimizer
 from itertools import count
 import numpy as np
 import os
-os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
 
-def entropy(p):
-    """Calculates entropy as H = - sum_i p_i * log(p_i) while assuming base e. Refer for further details to 
-    'Deep Reinforcement Learning' p.111.
-    See also the paper by Duan et al. 'Benchmarking Deep Reinforcement Learning for Continuous Control'."""
-    return np.sum(p * np.ln(p))
+def create_model(no_obs, no_actions, no_hidden_layers = 64):
 
-def entropy_optimalisation(theta):
-    pass
+        return nn.Sequential(
+        nn.Linear(in_features=no_obs,
+                  out_features=no_hidden_layers),
+        nn.ReLU(),
+        nn.Linear(in_features=no_hidden_layers,
+                  out_features=no_actions),
+    )
 
+
+def reinforce(model, observation):
+    observation_tensor = torch.as_tensor(observation, dtype=torch.float32)
+    logits = model(observation_tensor)
+
+    # Categorical will also normalize the logits for us
+    return Categorical(logits=logits)
+
+def choose_action(pi):
+    a = pi.sample().item()
+    log_probability_action = pi.log_prob(a)
+
+    return a, log_probability_action
+
+def get_loss(eps_log_prob, eps_actions_rewards):
+    return -(eps_log_prob * eps_actions_rewards).mean()
+
+
+def train_model(env, model, optimizer, max_timesteps = 2000, eps_timesteps = 200):
+
+    returns = []
+    log_probs = []
+    rewards = []
+    total_time = 0
+    while True:
+        if total_time > max_timesteps:
+            break
+        eps_reward = 0
+        obs = env.reset()
+        for i in range(eps_timesteps):
+            policy = reinforce(model, obs)
+            action, log_probability_action = get_action(policy)
+            obs, reward, done, _ = env.step(action)
+
+            eps_reward += reward
+
+            log_probs.append(log_probability_action)
+
+            # Finish the action loop if this episode is done
+            if done == True:
+                # Add one reward per timestep
+                for _ in range(timestep + 1):
+                    rewards.append(episode_reward)
+
+                break
+        returns.append(eps_reward)
+    loss = get_loss(torch.stack(log_probs),torch.as_tensor(rewards, dtype=torch.float32))
+
+
+    epoch_loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+
+    return np.mean(epoch_returns)
+
+def train(epochs=40) -> None:
+    """Train a Vanilla Policy Gradient model on CartPole
+
+    Args:
+        epochs (int, optional): The number of epochs to run for. Defaults to 50.
+    """
+
+    # Create the Gym Environment
+    env = gym.make("LunarLander-v2", render_mode="human")
+
+
+    # Create the MLP model
+    number_observation_features = env.observation_space.shape[0]
+    number_actions = env.action_space.n
+    model = create_model(number_observation_features, number_actions)
+
+    # Create the optimizer
+    optimizer = Adam(model.parameters(), 1e-2)
+
+    # Loop for each epoch
+    for epoch in range(epochs):
+        average_return = train_model(env, model, optimizer)
+        print('epoch: %3d \t return: %.3f' % (epoch, average_return))
+
+
+if __name__ == '__main__':
+    train()
 
 def main():
     #print('Device is:{}'.format(torch.cuda.get_device_name(0)))
     env = gym.make("LunarLander-v2", render_mode="human")
-
-
-    observation, info = env.reset()
-    env.render()
-    for _ in range(1000):
-        action = env.action_space.sample()
-        observation, reward, terminated, truncated, info = env.step(action)
-
-    if terminated or truncated:
-        observation, info = env.reset()
-    env.close()
+    
 
 if __name__ == "__main__":
     main()
