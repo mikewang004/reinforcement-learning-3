a3b57a99cb1fe5c834f729fcc001f01d06a3cdde
Bernard_ <99722688+XblaDeZ@users.noreply.github.com>
2024-04-25 21:12:41 +0200

    eindelijk doet deze mapangpangcode het, izjen bizjen. Maar hij heeft nu entropy regularization en n-step bootstrapping.

--- Bernard.py
+++ Bernard.py
@@ -40,8 +40,16 @@ class ACModel(nn.Module):
         entropy = dist.entropy().mean()
         return entropy
 
-    def loss(self, gamma=0.99, entropy_weight=0.01):
+    def calculate_n_step_returns(self, n=5, gamma=0.99):
+        n_step_returns = []
+        rewards = (self.rewards - np.mean(self.rewards)) / (np.std(self.rewards))
+        for i in reversed(range(len(rewards) - n + 1)):
+            n_step_return = sum([rewards[i + j] * (gamma ** j) for j in range(n)])
+            n_step_returns.insert(0, n_step_return)  # Prepend to the beginning
+        return n_step_returns
 
+    def loss(self, gamma=0.99, entropy_weight=0.01, n_steps=5):
+        '''
         # Calculate discounted rewards
         rewards = []
         discounted_reward = 0
@@ -51,12 +59,15 @@ class ACModel(nn.Module):
 
         rewards = torch.tensor(rewards)
         rewards = (rewards - rewards.mean()) / rewards.std()
+        '''
+        n_step_returns = self.calculate_n_step_returns(n=n_steps, gamma=gamma)
+        n_step_returns = torch.tensor(n_step_returns)
 
         # Calculate loss using advantage
         loss = 0
-        for log_prob, value, reward in zip(self.log_probs, self.state_values, rewards):
-            advantage = reward - value.item()
-            loss += (-log_prob * advantage) + F.smooth_l1_loss(value, reward)
+        for log_prob, value, n_step_return in zip(self.log_probs, self.state_values, n_step_returns):
+            advantage = n_step_return - value.item()
+            loss += (-log_prob * advantage) + F.smooth_l1_loss(value, n_step_return)
 
         # Add entropy term
         entropy = self.calculate_entropy(torch.stack(self.log_probs))
@@ -70,7 +81,9 @@ class ACModel(nn.Module):
         self.rewards.clear()
 
 
-def train(render = False, gamma=0.99, lr=0.02, betas=(0.9, 0.999), entropy_weight=0.01, num_episodes=1000, max_steps=10000, print_interval=10):
+def train(render = False, gamma=0.99, lr=0.02, betas=(0.9, 0.999),
+          entropy_weight=0.01, n_steps=5, num_episodes=1000,
+          max_steps=10000, print_interval=10):
     if render:
         env = gym.make("LunarLander-v2", render_mode="human")
         env.metadata['render_fps'] = 2048
@@ -93,7 +106,7 @@ def train(render = False, gamma=0.99, lr=0.02, betas=(0.9, 0.999), entropy_weigh
                 break
 
         optimizer.zero_grad()
-        loss = model.loss(gamma, entropy_weight=0.01)
+        loss = model.loss(gamma, entropy_weight=entropy_weight, n_steps=n_steps)
         loss.backward()
         optimizer.step()
         model.clear()
@@ -104,7 +117,7 @@ def train(render = False, gamma=0.99, lr=0.02, betas=(0.9, 0.999), entropy_weigh
             break
 
         if i % print_interval == 0:
-            running_reward = running_reward / 10
+            running_reward = running_reward / print_interval
             print('Episode {}\tmean reward: {:.2f}'.format(i + 1, running_reward))
             running_reward = 0
 
@@ -112,10 +125,10 @@ def train(render = False, gamma=0.99, lr=0.02, betas=(0.9, 0.999), entropy_weigh
 if __name__ == '__main__':
     train(render = True,
           gamma=0.99,
-          lr=0.02,
+          lr=0.01,
           betas=(0.9, 0.999),
           entropy_weight=0.01,
+          n_steps=10,
           num_episodes=1000,
           max_steps=10000,
           print_interval=10)
-
