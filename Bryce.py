b4ff5ab9334116dca2e397d5731b583e5bb35edb
BryceBenz <77013951+BryceBenz@users.noreply.github.com>
2024-05-06 17:41:34 +0200

    klees is kaar
    nu heeft ie ook N_bootstrap in de filename

--- Bryce.py
+++ Bryce.py
@@ -219,11 +219,14 @@ def train(render = False, gamma=0.99, lr=0.02, betas=(0.9, 0.999),
           num_episodesstr = str(num_episodes).replace(".","__")
           methodstr = str(method).replace(".","__")
           use_baselinestr = str(use_baseline).replace(".","__")
+          N_bootstrapstr = str(N_bootstrap).replace(".","__")
           DateTime = str(datetime.datetime.now().strftime("%y-%m-%d_%H-%M-%S")).replace(".","__")
-          with open(str('/home/Rewards_gamma-'+gammastr+'_lr-'+lrstr+
+          with open(str('/home/Rewards' +
+                    '_gamma-'+gammastr+'_lr-'+lrstr+
                     '_betas-'+betasstr+'_entropy-'+entropy_weightstr+
                     '_numepisodes-'+num_episodesstr+
                     '_method-'+methodstr+'_usebaseline-'+use_baselinestr+
+                    '_N_bootstrap-' + N_bootstrapstr +
                     '_'+DateTime+'.csv'), 'w') as file:
               for reward in rewards_lst:
                   file.write(str(reward) + '\n')
@@ -281,11 +284,14 @@ def train(render = False, gamma=0.99, lr=0.02, betas=(0.9, 0.999),
           num_episodesstr = str(num_episodes).replace(".","__")
           methodstr = str(method).replace(".","__")
           use_baselinestr = str(use_baseline).replace(".","__")
+          N_bootstrapstr = str(N_bootstrap).replace(".","__")
           DateTime = str(datetime.datetime.now().strftime("%y-%m-%d_%H-%M-%S")).replace(".","__")
-          with open(str('Rewards_gamma-'+gammastr+'_lr-'+lrstr+
+          with open(str('/home/Rewards' +
+                    '_gamma-'+gammastr+'_lr-'+lrstr+
                     '_betas-'+betasstr+'_entropy-'+entropy_weightstr+
                     '_numepisodes-'+num_episodesstr+
                     '_method-'+methodstr+'_usebaseline-'+use_baselinestr+
+                    '_N_bootstrap-' + N_bootstrapstr +
                     '_'+DateTime+'.csv'), 'w') as file:
               for reward in model.rewards_log:
                   file.write(str(reward) + '\n')
